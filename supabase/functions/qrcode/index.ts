f29b699873fb46df1df05015ce72f631b4910d79
Thomas Guillet <thomas@palya.fr>
2024-09-18 14:33:18 +0200

    Ajoute la route /qrcode dans supabase

---
+++ supabase/functions/qrcode/index.ts
@@ -0,0 +1,16 @@
+import { corsHeaders } from "../_shared/cors.ts";
+
+Deno.serve(async (req) => {
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
+
+  return new Response("OK", {
+    headers: {
+      ...corsHeaders,
+      "Content-Type": "text/plain",
+      location: "https://zsms.fr/mobile",
+    },
+    status: 302,
+  });
+});
