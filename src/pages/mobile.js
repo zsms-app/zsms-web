b8e340ec5c864f19ca4270e1048a3bbefd56bda6
Thomas Guillet <thomas@palya.fr>
2024-09-18 15:12:25 +0200

    Ajoute la page pour guider l'installation sur téléphone

---
+++ src/pages/mobile.js
@@ -0,0 +1,41 @@
+import Link from "next/link";
+
+export default function Mobile() {
+  return (
+    <>
+      <section className="section">
+        <div className="container">
+          <h1 className="title">
+            <Link href="/">zSMS</Link>{" "}
+          </h1>
+          <p className="subtitle">
+            Envoyez des SMS simplement depuis votre ordinateur !
+          </p>
+        </div>
+      </section>
+      <section className="section logos">
+        <div className="container">
+          <p>
+            Vous pouvez télécharger l'application sur{" "}
+            <strong>Google Play</strong> :
+            <br />
+            <a href="https://play.google.com/apps/internaltest/4700943393145190183">
+              <img src="/google-play.png" />
+            </a>
+            <br />
+            Si vous préférez éviter Google Play, vous pouvez installer
+            l'application directement <strong>à partir du fichier APK</strong> :
+            <br />
+            <a href="https://zsms-app.github.io/zsms-artefacts/app-release-0.0.4.apk">
+              <img src="/apk.png" />
+            </a>
+          </p>
+          <p>
+            En cas de difficulté ou de question, n'hésitez pas à nous contacter
+            à l'adresse électronique suivante : contact@zsms.fr
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
