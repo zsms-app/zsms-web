4a09b54ef291611d9a6ae425a64a488157c64e18
Thomas Guillet <thomas@palya.fr>
2024-11-18 14:24:15 +0100

    Log le certificat pour debug

--- supabase/functions/_shared/messaging.ts
+++ supabase/functions/_shared/messaging.ts
@@ -1,12 +1,14 @@
 import { initializeApp, cert } from "npm:firebase-admin/app";
 import { getMessaging, AndroidConfig } from "npm:firebase-admin/messaging";
 
+const credential = cert({
+  projectId: Deno.env.get("GOOGLE_APPLICATION_PROJECT_ID"),
+  clientEmail: Deno.env.get("GOOGLE_APPLICATION_CLIENT_EMAIL"),
+  privateKey: Deno.env.get("GOOGLE_APPLICATION_PRIVATE_KEY"),
+});
+console.log(credential);
 const app = initializeApp({
-  credential: cert({
-    projectId: Deno.env.get("GOOGLE_APPLICATION_PROJECT_ID"),
-    clientEmail: Deno.env.get("GOOGLE_APPLICATION_CLIENT_EMAIL"),
-    privateKey: Deno.env.get("GOOGLE_APPLICATION_PRIVATE_KEY"),
-  }),
+  credential,
   databaseURL: `https://${Deno.env.get("GOOGLE_APPLICATION_DATABASE")}.firebaseio.com`,
 });
 
