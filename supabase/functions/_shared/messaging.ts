1d8fb4d4e6c1ea0ac78999ebd4f304b7437f67e8
Thomas Guillet <thomas@palya.fr>
2024-11-18 16:10:51 +0100

    Met à jour la version de firebase admin et ajuste la création du certificat

--- supabase/functions/_shared/messaging.ts
+++ supabase/functions/_shared/messaging.ts
@@ -1,12 +1,11 @@
-import { initializeApp, cert } from "npm:firebase-admin/app";
-import { getMessaging, AndroidConfig } from "npm:firebase-admin/messaging";
+import { initializeApp, cert } from "npm:firebase-admin@13/app";
+import { getMessaging, AndroidConfig } from "npm:firebase-admin@13/messaging";
 
 const credential = cert({
-  projectId: Deno.env.get("GOOGLE_APPLICATION_PROJECT_ID"),
-  clientEmail: Deno.env.get("GOOGLE_APPLICATION_CLIENT_EMAIL"),
-  privateKey: Deno.env.get("GOOGLE_APPLICATION_PRIVATE_KEY"),
+  project_id: Deno.env.get("GOOGLE_APPLICATION_PROJECT_ID"),
+  client_email: Deno.env.get("GOOGLE_APPLICATION_CLIENT_EMAIL"),
+  private_key: Deno.env.get("GOOGLE_APPLICATION_PRIVATE_KEY"),
 });
-console.log(credential);
 const app = initializeApp({
   credential,
   databaseURL: `https://${Deno.env.get("GOOGLE_APPLICATION_DATABASE")}.firebaseio.com`,
